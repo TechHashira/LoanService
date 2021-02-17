603204077f990bd5025c8aafb8de873c5fdf3a64
Eduardo Lemus <rodrigolemus3098@gmail.com>
2021-02-17 01:41:15 -0600

    Ormconfig added and migrations fixed

--- src/modules/app/app.module.ts
+++ src/modules/app/app.module.ts
@@ -1,15 +1,32 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AdminModule } from '../admin/admin.module';
-import { config } from './orm/ormconfig';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
-    TypeOrmModule.forRoot(config),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        type: 'mysql',
+        host: configService.get('DB_HOST'),
+        port: configService.get<number>('DB_PORT'),
+        username: configService.get('DB_USERNAME'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_NAME'),
+        entities: [__dirname + '/../../modules/**/*.entity{.ts,.js}'],
+        migrations: [__dirname + '/../../migrations/*{.ts,.js}'],
+        cli: {
+          migrationsDir: 'src/migrations',
+        },
+        migrationsRun: true,
+        synchronize: false,
+      }),
+      inject: [ConfigService],
+    }),
     AdminModule,
   ],
   controllers: [],
--- src/modules/user/entities/user.entity.ts
+++ src/modules/user/entities/user.entity.ts
@@ -1,5 +1,4 @@
 import { RoleType } from 'src/common/constants';
-import { AdminEntity } from 'src/modules/admin/entities/admin.entity';
 import { LoanEntity } from 'src/modules/loan/entities/loan.entity';
 import { UserSavingEntity } from 'src/modules/saving/entities/user-saving.entity';
 import { WorkSheetUserEntity } from 'src/modules/worksheet/entities/worksheetUser.entity';
