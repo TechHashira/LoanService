0206b6bbc7354d594312f80c65cedb5323a3e3bb
Rodrigo Lemus <rodrigolemus3098@gmail.com>
2021-02-25 23:39:37 -0600

    cors added

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import { AppModule } from './modules/app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
+  app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
 
   const configService = app.get(ConfigService);
