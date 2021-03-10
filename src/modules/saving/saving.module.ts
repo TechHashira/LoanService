aae7578f3a1f7c85861dd4f4746f3ad88891b3c3
Rodrigo Lemus <rodrigolemus3098@gmail.com>
2021-03-10 09:51:35 -0600

    feat: savings get endpoints added

--- src/modules/saving/saving.module.ts
+++ src/modules/saving/saving.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserModule } from '../user/user.module';
+import { UserSavingController } from './controllers/saving.controller';
 import { UserSavingRepository } from './repositories/userSaving.repository';
 import { UserSavingService } from './services/userSaving.service';
 
@@ -8,5 +9,6 @@ import { UserSavingService } from './services/userSaving.service';
   imports: [UserModule, TypeOrmModule.forFeature([UserSavingRepository])],
   exports: [UserSavingService],
   providers: [UserSavingService],
+  controllers: [UserSavingController],
 })
 export class SavingModule {}
--- src/modules/saving/services/userSaving.service.ts
+++ src/modules/saving/services/userSaving.service.ts
@@ -1,7 +1,10 @@
 import { Injectable } from '@nestjs/common';
+import { QueryParamsDto } from 'src/common/dtos/queryOptions.dto';
 import { CreatedFailedException } from 'src/modules/user/exceptions/createdFailed.exception';
+import { UserNotFoundException } from 'src/modules/user/exceptions/userNotFound.exception';
 import { UserService } from 'src/modules/user/services/user.service';
 import { CreateSavingDto } from '../dtos/createSaving.dto';
+import { UserSavingEntity } from '../entities/user-saving.entity';
 import { UserSavingRepository } from '../repositories/userSaving.repository';
 
 @Injectable()
@@ -23,6 +26,45 @@ export class UserSavingService {
       return saving;
     } catch ({ message }) {
       throw new CreatedFailedException(message);
+    }
+  }
+  public async getAllSavings(
+    queryParamsDto: QueryParamsDto,
+  ): Promise<Array<UserSavingEntity>> {
+    const { take } = queryParamsDto;
+    const queryBuilder = this._userSavingRepository.createQueryBuilder(
+      'saving_alias',
+    );
+    const savings = queryBuilder.take(take).getMany();
+
+    return savings;
+  }
+
+  public async getUserSavingById(
+    userSavingId: number,
+  ): Promise<UserSavingEntity> {
+    const queryBuilder = this._userSavingRepository.createQueryBuilder(
+      'saving_alias',
+    );
+    return await queryBuilder
+      .where('saving_alias.id = :userSavingId', { userSavingId })
+      .getOne();
+  }
+
+  public async getAllSavingsByUserId(
+    id: number,
+  ): Promise<Array<UserSavingEntity>> {
+    const queryBuilder = this._userSavingRepository.createQueryBuilder(
+      'saving_alias',
+    );
+    try {
+      const savings = await queryBuilder
+        .where('saving_alias.userId = :id', { id })
+        .getMany();
+
+      return savings;
+    } catch ({ message }) {
+      throw new UserNotFoundException(message);
     }
   }
 }
