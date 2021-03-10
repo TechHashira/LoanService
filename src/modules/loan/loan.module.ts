461d7537962efe99290db22c22f6d9b0b27b2bd2
Rodrigo Lemus <rodrigolemus3098@gmail.com>
2021-03-10 02:38:55 -0600

    Create Loan fixed

--- src/modules/loan/loan.module.ts
+++ src/modules/loan/loan.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserModule } from '../user/user.module';
 import { LoanRepository } from './repositories/loan.repository';
 import { LoanService } from './services/loan.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([LoanRepository])],
+  imports: [UserModule, TypeOrmModule.forFeature([LoanRepository])],
   exports: [LoanService],
   providers: [LoanService],
 })
--- src/modules/loan/services/loan.service.ts
+++ src/modules/loan/services/loan.service.ts
@@ -1,15 +1,21 @@
 import { Injectable } from '@nestjs/common';
 import { CreatedFailedException } from 'src/modules/user/exceptions/createdFailed.exception';
+import { UserService } from 'src/modules/user/services/user.service';
 import { CreateLoanDto } from '../dtos/createLoan.dto';
 import { LoanRepository } from '../repositories/loan.repository';
 
 @Injectable()
 export class LoanService {
-  constructor(private _loanRepository: LoanRepository) {}
+  constructor(
+    private _loanRepository: LoanRepository,
+    private _userService: UserService,
+  ) {}
 
   public async createLoan(createLoanDto: CreateLoanDto) {
     try {
-      const loan = this._loanRepository.create(createLoanDto);
+      const { userId, ...auxCreateLoanDto } = { ...createLoanDto };
+      const user = await this._userService.findUserById(userId);
+      const loan = this._loanRepository.create({ ...auxCreateLoanDto, user });
       await this._loanRepository.save(loan);
       return loan;
     } catch ({ message }) {
